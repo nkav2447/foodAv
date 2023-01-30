435386da3b2287de66f7bf240adb39801501c347
nkav2447 <nkav2447@gmail.com>
2023-01-30 09:10:46 -0300

    setup the routes for each category of food

--- index.js
+++ index.js
@@ -27,6 +27,90 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
+app.get('/Beef', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Beef', { meals, mealsLength })
+})
+
+app.get('/Chicken', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Chicken', { meals, mealsLength })
+})
+
+app.get('/Dessert', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Dessert', { meals, mealsLength })
+})
+
+app.get('/Lamb', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Lamb`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Lamb', { meals, mealsLength })
+})
+
+app.get('/Miscellaneous', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Miscellaneous`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Miscellaneous', { meals, mealsLength })
+})
+
+app.get('/Pasta', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Pasta', { meals, mealsLength })
+})
+
+app.get('/Pork', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Pork`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Pork', { meals, mealsLength })
+})
+
+app.get('/Seafood', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Seafood', { meals, mealsLength })
+})
+
+app.get('/Side', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Side`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Side', { meals, mealsLength })
+})
+
+app.get('/Starter', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Starter`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Starter', { meals, mealsLength })
+})
+
+app.get('/Vegan', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Vegan', { meals, mealsLength })
+})
+
+app.get('/Vegetarian', async function (req, res) {
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian`)
+    const meals = resCategory.data.meals
+    const mealsLength = meals.length
+    res.render('category/Vegetarian', { meals, mealsLength })
+})
+
 app.listen(port, () => {
     console.log(`port started on port ${port}`)
 })
