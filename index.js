62ed7866b5bfa7dec65845ac28f3fda7d10e9b27
nkav2447 <nkav2447@gmail.com>
2023-01-30 08:48:38 -0300

    added a setup for views folder and ejs utlization

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const app = express();
 const port = process.env.PORT || 3000
 
 
+app.engine('ejs', ejsMate)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/home', (req, res) => {
+    res.render('home')
+})
+
 app.listen(port, () => {
     console.log(`port started on port ${port}`)
 })
