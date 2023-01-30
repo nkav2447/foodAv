cd0eb8de892746d10a7b13ccc6c56b8022f6082e
nkav2447 <nkav2447@gmail.com>
2023-01-30 09:12:36 -0300

    setup the routes for the meals corresponding to each category of the food

--- index.js
+++ index.js
@@ -111,6 +111,151 @@ app.get('/Vegetarian', async function (req, res) {
     res.render('category/Vegetarian', { meals, mealsLength })
 })
 
+app.get('/meals/beefMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/beefMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/chickenMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/chickenMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/dessertMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/dessertMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/lambMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Lamb`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/lambMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/miscellaneousMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Miscellaneous`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/miscellaneousMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/pastaMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Pasta`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/pastaMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/porkMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Pork`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/porkMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/seafoodMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/seafoodMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/sideMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Side`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/sideMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/starterMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Starter`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/starterMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/veganMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegan`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/veganMeal`, { foundMeal, yt_id })
+})
+
+app.get('/meals/vegetarianMeal/:id', async (req, res) => {
+    const { id } = req.params
+    const resCategory = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian`)
+    const meals = resCategory.data.meals
+    const meal = meals.find(m => m.idMeal === id)
+    const idMeal = meal.idMeal
+    const resMeal = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    const foundMeal = resMeal.data.meals[0]
+    const yt_id = foundMeal.strYoutube.substring(32)
+    res.render(`meals/vegetarianMeal`, { foundMeal, yt_id })
+})
+
+
 app.listen(port, () => {
     console.log(`port started on port ${port}`)
 })
